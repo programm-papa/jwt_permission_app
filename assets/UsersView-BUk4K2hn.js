5267d8579cafc21ba67ce370518b2a7278c6578c
Pavel Kuznecov <pavel.k@wemakefab.ru>
2024-06-22 16:30:41 +0300

    added dist update sw

---
+++ assets/UsersView-BUk4K2hn.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o,c as s,a as t}from"./index-DSwExwF9.js";const c={},n=t("h1",null,"Данная страница доступна редакторам и администраторам",-1),r=[n];function a(_,i){return o(),s("main",null,r)}const m=e(c,[["render",a]]);export{m as default};
